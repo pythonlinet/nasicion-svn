1182eaa8e4f13db150612b24fc12065454ebc207
nasicion <nasicion@7a0531c8-303a-11de-874f-bb9810f3bc79>
2009-04-24 00:53:57 +0000

    20090423_2153 Culminacion de varios metodos, ya se pasan los 2 primeros TestCases :D

--- Obligatorio1/src/uy/edu/ucu/pii/obligatorio1/gnasi/datos/Pelicula.java
+++ Obligatorio1/src/uy/edu/ucu/pii/obligatorio1/gnasi/datos/Pelicula.java
@@ -1,10 +1,11 @@
 package uy.edu.ucu.pii.obligatorio1.gnasi.datos;
 
 import uy.edu.ucu.pii.obligatorio1.gnasi.datos.lista.TLista;
+import uy.edu.ucu.pii.obligatorio1.gnasi.datos.lista.TNodo;
 
 public class Pelicula {
 	private String nomPelicula;
-	private TLista actores;
+	private TLista actores = new TLista();
 	
 	public String getNombre() {
 		return nomPelicula;
@@ -22,14 +23,29 @@ public class Pelicula {
 	}
 	
 	/**
-	 * 
+	 * Agrega un actor a la pelicula
 	 * @param actor
-	 * @return
+	 * @return true - si se inserto correctamente; false - si el actor ya esta asociado a la pelicula
 	 */
 	public boolean agregarActor(Actor actor){
 		return actores.insertarOrdenado(actor.getNombre(), actor);
 	}
 	
+	/**
+	 * Busca un actor dentro de la pelicula
+	 * @param nomActor
+	 * @return unActor - si el actor esta asociado a dicha pelicula; null - si el actor no esta asociado a la pelicula
+	 */
+	public Actor buscarActor(String nomActor){
+		Actor result = null;
+		
+		TNodo busqueda = actores.buscarNodo(nomActor);
+		//Revisamos que no sea null porque si queremos hacer getElemento de un nodo nulo Java responde con NullPointerException
+		if(busqueda != null)
+			result = (Actor)busqueda.getElemento();
+
+		return result;
+	}
 	
 	
 	
