dd37e3e1173fcc56d90d38adee404b264a0aeed6
nasicion <nasicion@7a0531c8-303a-11de-874f-bb9810f3bc79>
2009-05-17 15:01:38 +0000

    los 3 primeros del test case

--- trunk/TDAGrafo/src/uy/edu/ucu/pii/obligatorio1/grupo14/datos/grafo/TGrafo.java
+++ trunk/TDAGrafo/src/uy/edu/ucu/pii/obligatorio1/grupo14/datos/grafo/TGrafo.java
@@ -2,47 +2,257 @@ package uy.edu.ucu.pii.obligatorio1.grupo14.datos.grafo;
 
 import uy.edu.ucu.pii.obligatorio1.grupo14.datos.lista.TLista;
 import uy.edu.ucu.pii.obligatorio1.grupo14.datos.lista.TNodo;
+import uy.edu.ucu.pii.obligatorio1.grupo14.datos.grafo.TArista;
+
 /**
  * Clase para la implementacion de Grafos.
  * 
  * @author Grupo14
- *
+ * 
  */
 public class TGrafo {
-	TLista vertices;
+	private static final Comparable INFINITO = null;
+
+	private TLista vertices;
+
+	private Comparable[][] mAdyacencia;
+	private Comparable[][] mFloyd;
+	private boolean regenMatriz;
+	private int cantVertices = 0;
+
+	public int getCantVertices() {
+		return cantVertices;
+	}
+
+	private void setCantVertices(int cantVertices) {
+		this.cantVertices = cantVertices;
+	}
+
+	public TGrafo() {
+		this.vertices = new TLista();
+		this.cantVertices = 0;
+		this.regenMatriz = true;
+
+	}
 
 	/**
-	 * Metodo para insertar un nuevo vertice en el grafo 
+	 * Metodo para insertar un nuevo vertice en el grafo
+	 * 
 	 * @param etiqueta
-	 * @return
+	 * @return true - si el vertice se inserto correctamente; false - si el
+	 *         vertice ya existe
 	 */
 	public boolean insertarVertice(Comparable etiqueta) {
 		// El metodo insertar de TLista no permite insercion de datos
 		// duplicados, por lo tanto no es necesario verificar que el elemento no
 		// exista
-		return vertices.insertarOrdenado(etiqueta, new TVertice(etiqueta));
+		boolean salida = vertices.insertarOrdenado(etiqueta, new TVertice(
+				etiqueta, cantVertices));
+		// Si se realiza la insercion aumentamos la cantidad de vertices en el
+		// grafo
+		setCantVertices(salida ? getCantVertices() + 1 : getCantVertices());
+		// Si se realizo la insercion marcamos que la matriz tiene que ser
+		// regenerada la proxima vez que se la quiera accesar
+		if(salida){
+			this.regenMatriz = salida;
+		}
+
+		return salida;
 
 	}
+
 	/**
-	 *  Metodo para agregar una adyacencia 
-	 * @param origen vertice de origen
-	 * @param destino vertice de destino
-	 * @param costo costo desde el origen hasta el destino
-	 * @return true - si se pudo agregar la adyacencia; false - si ya existia una adyacencia o si uno de los 
+	 * Metodo para agregar una adyacencia
+	 * 
+	 * @param origen
+	 *            vertice de origen
+	 * @param destino
+	 *            vertice de destino
+	 * @param costo
+	 *            costo desde el origen hasta el destino
+	 * @return true - si se pudo agregar la adyacencia; false - si ya existia
+	 *         una adyacencia o si uno de los
 	 */
-	public boolean insertarAdyacencia(Comparable origen, Comparable destino, Comparable costo){
+	public boolean insertarAdyacencia(Comparable origen, Comparable destino,
+			Comparable costo) {
 		boolean salida = false;
-		
+
 		TNodo nodoOrigen = vertices.buscarNodo(origen);
-		//Si el nodo de origen existe
-		if(nodoOrigen != null){
+		// Si el nodo de origen existe
+		if (nodoOrigen != null) {
 			TNodo nodoDestino = vertices.buscarNodo(destino);
-			//Si el nodo destino existe
-			if(nodoDestino != null){
-				//Demasiados casteos??
-				salida = ((TVertice)nodoOrigen.getElemento()).ingresarAdyacencia((TVertice)nodoDestino.getElemento(), costo);
+			// Si el nodo destino existe
+			if (nodoDestino != null) {
+				// Demasiados casteos?? :P
+				// No es necesario hacer un control de si ya existe la
+				// adyacencia porque el insertar de la lista ya lo hace
+				salida = ((TVertice) nodoOrigen.getElemento())
+						.ingresarAdyacencia((TVertice) nodoDestino
+								.getElemento(), costo);
+				// Si se realizo la insercion de adyacencia marcamos que la
+				// matriz tiene que ser regenerada la proxima vez que se la
+				// quiera accesar
+				if(salida){
+					this.regenMatriz = salida;
+				}
 			}
 		}
+		return salida;
+	}
+
+	/**
+	 * Floyd
+	 * 
+	 * @param etiquetaOrigen
+	 * @param etiquetaDestino
+	 * @return
+	 */
+	public boolean existeCamino(Comparable etiquetaOrigen, Comparable etiquetaDestino) {
+		implementacionFloyd();
+		int origen = getPosMatriz(etiquetaOrigen);
+		int destino = getPosMatriz(etiquetaDestino);
+		return  mFloyd[origen][destino] != INFINITO?true:false;
+	}
+
+	public Comparable[] mejorCamino(Comparable etiquetaOrigen,
+			Comparable etiquetaDestino) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object centroDelGrafo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Metodo para saber si existe un vertice con una X etiqueta
+	 * 
+	 * @param etiqueta etiqueta del vertices
+	 * @return true - si el vertice existe en el grafo; false - si el vertice no existe en el grafo
+	 */
+	public boolean existeVertice(String etiqueta) {
+		return vertices.buscarNodo(etiqueta) == null ? false : true;
+	}
+
+	/**
+	 * Metodo para saber si dos vectores ORIGEN y DESTINO son adyacentes
+	 * 
+	 * @param etiquetaOrigen etiqueta del nodo origen
+	 * @param etiquetaDestino etiqueta del nodo destino
+	 * @return true - si existe la adyacencia; false - si no existe una adyacencia entre ambos vertices
+	 */
+	public boolean existeAdyacencia(String etiquetaOrigen,	String etiquetaDestino) {
+		boolean salida = false;
+		TNodo origen = vertices.buscarNodo(etiquetaOrigen);
+		// Si existe el origen
+		if (origen != null) {
+			salida = ((TVertice) origen.getElemento()).existeAdyacencia(etiquetaDestino);
+		}
+		return salida;
+	}
+
+	/**
+	 * Inicializa la matriz de adyacencia
+	 */
+	private void inicializarMatriz() {
+		int tamanio = vertices.getTamanio();
+		this.mAdyacencia = new Comparable[tamanio][tamanio];
+	}
+
+	/**
+	 * Carga la matriz de adyacencia Para el caso que sea desde en Vertice X
+	 * hacia el mismo el costo sera 0 Cuando no haya una adyacencia se dejara el
+	 * valor null y se lo manejara como si fuera INFINITO, es decir que no hay
+	 * adyacencia entre ambos vertices
+	 */
+	private void cargarMatrizDeAdyacencia() {
+		inicializarMatriz();
+		int cantVertices = vertices.getTamanio();
+		TVertice vertice;
+		// Usado para guardar las aristas
+		TArista adyacencia;
+		int cantAdyacencias;
+		for (int i = 0; i < cantVertices; i++) {
+			vertice = (TVertice) vertices.recuperar(i).getElemento();
+			// Cantida de adyacencias del vertice
+			cantAdyacencias = vertice.getAdyacentes().getTamanio();
+			
+			// Cuando sea el costo de un vertice x hacia el mismo, seteamos el costo en 0
+			mAdyacencia[i][i] = 0;
+			
+			// Recorremos las adyacencias del vertice
+			for (int j = 0; j < cantAdyacencias; j++) {
+
+				// Guardo la referencia a la arista
+				adyacencia = ((TArista) vertice.getAdyacentes().recuperar(j).getElemento());
+				mAdyacencia[i][adyacencia.destino.getPosMatriz()] = adyacencia.costo;
+			}
+		}
+		this.regenMatriz = false;
+	}
+
+	/**
+	 * Metodo para saber la posicion que tiene un vertice en la matriz
+	 * 
+	 * @param etiqueta etiqueta del vertice del que se quiere saber la posicion
+	 * @return unNumero - la posición del vertice dentro de la matriz de adyacencia; -1 - si el vertice no existe
+	 */
+	private int getPosMatriz(Comparable etiqueta) {
+		int salida = -1;
+		// Buscamos el vertice
+		TVertice vertice = (TVertice) vertices.buscarNodo(etiqueta).getElemento();
+		// Si el vertice existe guardamos la posición que tiene en la matriz
+		salida = vertice != null ? vertice.getPosMatriz() : salida;
+
+		return salida;
+	}
+
+	/**
+	 * Implementacion del algoritmo de Floyd
+	 * 
+	 * @return Comparable[][] - con los costos minimos de ir desde un Origen hasta un Destino
+	 */
+	private Comparable[][] implementacionFloyd() {
+		// Si la matriz fue marcada para regeneracion se la genera otra vez
+		//if (regenMatriz)
+			cargarMatrizDeAdyacencia();
+
+		Comparable[][] salida = this.mAdyacencia;
+		Integer aIJ;
+		Integer aIK;
+		Integer aKJ;
+
+		for (int k = 0; k < salida.length; k++) {
+			for (int i = 0; i < salida.length; i++) {
+				for (int j = 0; j < salida.length; j++) {
+					// Si ninguno de los dos es infinito(NULL)
+					if (salida[i][k] != INFINITO && salida[k][j] != INFINITO) {
+
+						aIK = (Integer)salida[i][k];
+						aKJ = (Integer)salida[k][j];
+						// Si el costo inicial no es INFINITO
+						if (salida[i][j] != INFINITO) {
+							// Para poder realizar la comparacion o la suma de
+							// los costos se tiene que pasar las variables de
+							// Comparable a otro Objeto que soporte estas
+							// operaciones
+							aIJ = (Integer) salida[i][j];
+							if (aIJ > aIK + aKJ) {
+								salida[i][j] = aIK + aKJ;
+							}
+						} else {
+							// Si resulta que el costo inicial era INFINITO tomo
+							// el costo de ir de I hasta K y luego desde K hasta
+							// el destino J
+							salida[i][j] = aIK + aKJ;
+						}
+					}
+				}
+			}
+		}
+		
+		this.mFloyd = salida;
 		return salida;
 	}
 }
---
+++ trunk/TDAGrafo/src/uy/edu/ucu/pii/util/tda/grafo/TGrafoTest.java
@@ -0,0 +1,105 @@
+package uy.edu.ucu.pii.util.tda.grafo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uy.edu.ucu.pii.obligatorio1.grupo14.datos.grafo.TGrafo;
+
+public class TGrafoTest {
+	private static final String[] VERTICES_INICIALES_1 = new String[]{"A","B","C"};
+	private static final String[] VERTICES_INICIALES_2 = new String[]{"A","B","C","D","E","F"};
+	private static final String[][] ARISTAS_INICIALES_2 = new String[][]{{"A","B"},{"B","C"},{"B","E"},{"C","D"},{"C","E"},{"E","F"}};
+	private static final Integer[] COSTOS_ARTISTAS_INICIALES_2 = new Integer[]{10,10,9,8,6,14};
+	
+	
+	private TGrafo g = null;
+	@Before
+	public void before() {
+		g = new TGrafo();
+	}
+	
+	@Test
+	public void testInsertaVertices() {
+		validarInsercionVertices(VERTICES_INICIALES_1);
+	}
+
+	@Test
+	public void testInsertaVerticesYAdyacencias() {
+		validarInsercionVertices(VERTICES_INICIALES_2);
+		validarInsercionAdyacencias(ARISTAS_INICIALES_2, COSTOS_ARTISTAS_INICIALES_2);
+	}
+
+	@Test
+	public void testExisteCamino() {
+		validarInsercionVertices(VERTICES_INICIALES_2);
+		validarInsercionAdyacencias(ARISTAS_INICIALES_2, COSTOS_ARTISTAS_INICIALES_2);
+		
+		assertTrue(g.existeCamino("A","B"));
+		assertFalse(g.existeCamino("F","A"));
+		assertTrue(g.existeCamino("C","F"));
+		assertTrue(g.existeCamino("B","F"));
+	}
+	
+	@Test
+	public void testMejorCamino() {
+		validarInsercionVertices(VERTICES_INICIALES_2);
+		validarInsercionAdyacencias(ARISTAS_INICIALES_2, COSTOS_ARTISTAS_INICIALES_2);
+		
+		String[] caminoEsperado = new String[] {"A","B","E","F"};
+		validarListasIguales("El mejor camino entre A y F debe ser "+caminoEsperado,caminoEsperado,g.mejorCamino("A", "F"));
+	}
+	
+	@Test
+	public void testCentro() {
+		validarInsercionVertices(VERTICES_INICIALES_2);
+		validarInsercionAdyacencias(ARISTAS_INICIALES_2, COSTOS_ARTISTAS_INICIALES_2);
+		
+		assertEquals(g.centroDelGrafo(),"F");
+	}
+
+	
+
+	@SuppressWarnings("unchecked")
+	private void validarListasIguales(String mensajeError,String[] array1, Comparable[] array2) {
+		assertNotNull(array1);
+		assertNotNull(array2);
+		assertEquals(array1.length, array2.length);
+		for (int i = 0; i < array1.length; i++) {
+			assertEquals(mensajeError, array1[i], array2[i]);
+		}
+		
+	}
+
+	private void validarInsercionVertices(String[] etiquetas){
+		for (int i = 0; i < etiquetas.length; i++) {
+			validarInsercionVertice(etiquetas[i]);
+		}
+	}
+	
+	private void validarInsercionVertice(String etiqueta) {
+		assertFalse(g.existeVertice(etiqueta));
+		assertTrue("Siempre debe ser posible insertar vertices en un grafo",g.insertarVertice(etiqueta));
+		assertFalse("No es posible insertar vertices repetidos",g.insertarVertice(etiqueta));
+		assertTrue("Es necesario que se encuentre un vertice que acabo de insertar!!",g.existeVertice(etiqueta));
+	}
+	
+	/**
+	 * @param adyacencias Arreglo con pares ordenados: etOrigen - etDestino (en forma de un array de dos posiciones)
+	 * @param costos
+	 */
+	private void validarInsercionAdyacencias(String[][] adyacencias, Integer[] costos){
+		for (int i = 0; i < adyacencias.length; i++) {
+			String[] adyacenciaActual = adyacencias[i];
+			validarInsercionAdyacencia(adyacenciaActual[0], adyacenciaActual[1], costos[i]);
+		}
+	}
+	
+	private void validarInsercionAdyacencia(String etiquetaOrigen, String etiquetaDestino, Integer costo){
+		assertFalse(g.existeAdyacencia(etiquetaOrigen, etiquetaDestino));
+		assertTrue("Siempre debe ser posible insertar adyacencias inexistentes a un grafo",g.insertarAdyacencia(etiquetaOrigen, etiquetaDestino, costo));
+		assertFalse("No es posible insertar adyacencias duplicadas",g.insertarAdyacencia(etiquetaOrigen, etiquetaDestino, costo));
+		assertTrue("Es necesario que se encuentre una adyacencia que acabo de insertar!!",g.existeAdyacencia(etiquetaOrigen, etiquetaDestino));
+	}
+}
