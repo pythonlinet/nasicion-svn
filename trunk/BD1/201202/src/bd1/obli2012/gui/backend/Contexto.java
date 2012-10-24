9a8b025d8d66ec45b9bd4d97929cf7d12ed4fcb2
nasicion@gmail.com <nasicion@gmail.com@7a0531c8-303a-11de-874f-bb9810f3bc79>
2012-10-24 19:04:27 +0000

    Refactor de estructura

---
+++ trunk/BD1/201202/src/bd1/obli2012/gui/backend/Contexto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd1.obli2012.gui.backend;
+
+/**
+ *
+ * @author gnasi
+ */
+public class Contexto {
+    
+    private Contexto instance;
+    
+    private Contexto(){
+        
+    }
+}
