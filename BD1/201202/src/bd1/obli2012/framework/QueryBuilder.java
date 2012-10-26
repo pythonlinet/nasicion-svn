9df56fa2da846bc42cff09de1d2afc96b6001574
nasicion <nasicion@gmail.com>
2012-10-26 16:12:30 +0000

    Aplicacion externa hace alta y prpara el form para modificar. Query builder varios cambios, mirar diff

--- BD1/201202/src/bd1/obli2012/framework/QueryBuilder.java
+++ BD1/201202/src/bd1/obli2012/framework/QueryBuilder.java
@@ -6,6 +6,7 @@ package bd1.obli2012.framework;
 
 import bd1.obli2012.framework.definicion.Columna;
 import bd1.obli2012.framework.definicion.Tabla;
+import java.util.List;
 
 /**
  *
@@ -13,6 +14,11 @@ import bd1.obli2012.framework.definicion.Tabla;
  */
 public class QueryBuilder {
 
+    /**
+     * Genera la query para obtener toda la informacion de una tablar
+     * @param tabla
+     * @return 
+     */
     public static String obtenerInformacionTabla(Tabla tabla) {
         StringBuilder sb = new StringBuilder("SELECT ");
         for(Columna c : tabla.getAttributes()) {
@@ -21,6 +27,46 @@ public class QueryBuilder {
         sb.replace(sb.length()-1, sb.length(), "");
         sb.append(" FROM ").append(tabla.getNombre()).append(";");
         
+        return sb.toString();
+    }
+
+    /**
+     * Genera la query para instertar en una tabla
+     * @param tabla
+     * @param valores 
+     */
+    public static String insertarEnTabla(Tabla tabla, String[] valores) {
+        StringBuilder sb = 
+                new StringBuilder("INSERT INTO ").
+                append(tabla.getNombre()).
+                append(" VALUES (");
+        for(String val :valores){
+            sb.append("'").append(val).append("'").append(",");
+        }
+        sb.replace(sb.length()-1, sb.length(), "");
+        sb.append(");");
+        return sb.toString();
+    }
+
+    public static String obtenerTuplaTabla(Tabla tabla, List<String> valoresPK) {
+        StringBuilder sb = new StringBuilder("SELECT ");
+        for(Columna c : tabla.getAttributes()) {
+            sb.append(c.getNombre()).append(",");
+        }
+        sb.replace(sb.length()-1, sb.length(), "");
+        sb.append(" FROM ").
+                append(tabla.getNombre()).
+                append(" WHERE ");
+        
+        
+        for (int i = 0; i < valoresPK.size(); i++) {
+            sb.append(tabla.getPrimaryKeys().get(i)).
+                    append("='").
+                    append(valoresPK.get(i)).
+                    append("' AND ");
+        }
+        sb.replace(sb.length()-4, sb.length(), "");
+        sb.append(";");
         return sb.toString();
     }
 }
